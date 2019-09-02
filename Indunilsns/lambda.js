7879270fa856bce4f0d5aef10ded5acc6a7a9442
Indunil1992 <indunil@adroitlogic.com>
2019-09-02 11:12:00 +0530

    1qa

--- Indunilsns/lambda.js
+++ Indunilsns/lambda.js
@@ -1,26 +1,23 @@
 let AWS = require('aws-sdk');
-let SL_AWS = require('slappforge-sdk-aws');
-const sqs = new SL_AWS.SQS(AWS);
+const sns = new AWS.SNS();
 
 exports.handler = function (event, context, callback) {
-    sqs.receiveMessage({
-        QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/testindunil`,
-        AttributeNames: ['All'],
-        MaxNumberOfMessages: '1',
-        VisibilityTimeout: '30',
-        WaitTimeSeconds: '0',
-        MessageAttributeNames: ['test1', '2', '3', '4']
-    }).promise()
-        .then(receivedMsgData => {
-            if (!!(receivedMsgData) && !!(receivedMsgData.Messages)) {
-                let receivedMessages = receivedMsgData.Messages;
-                receivedMessages.forEach(message => {
-                    // your logic to access each message through out the loop. Each message is available under variable message 
-                    // within this block
-                });
-            } else {
-                // No messages to process
+    sns.publish({
+        Message: 'test1',
+        MessageAttributes: {
+            'AWS.SNS.SMS.SMSType': {
+                'DataType': 'String',
+                'StringValue': 'Promotional'
+            },
+            'AWS.SNS.SMS.SenderID': {
+                'DataType': 'String',
+                'StringValue': 'ID 1'
             }
+        },
+        PhoneNumber: '+94713245242'
+    }).promise()
+        .then(data => {
+            // your code goes here
         })
         .catch(err => {
             // error handling goes here
